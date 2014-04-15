0b80b55508ab07c3187b984eff9bc9f6e315b261
will hunting <willhunting@wills-Mac.local>
2014-04-15 20:45:16 +0800

    Initial Commit

---
+++ machismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  machismo
+//
+//  Created by will hunting on 4/15/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ machismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  machismo
+//
+//  Created by will hunting on 4/15/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
